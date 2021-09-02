89b6052251fc41b80dc1536b583fcdf3db992e75
Yesh bourne <yeshgpu@gmail.com>
2021-09-02 13:51:41 -0700

    adding edgetpu compatible model

--- TensorFlow-2.0/workspace/training/tflite_convertor.py
+++ TensorFlow-2.0/workspace/training/tflite_convertor.py
@@ -1,34 +1,38 @@
-import tensorflow as tf
-import os
 import numpy as np
-import cv2
+import tensorflow as tf
 
-saved_model_dir = './exported-models/ptag-detector-model/ssd_mobilenet_v2_fpnlite_320x320/saved_model'
-images_path = './quant-images/'
-
-NORM_H = 320
-NORM_W = 320
+rep_ds = tf.data.Dataset.list_files("./quant-images/*.jpg")
+HEIGHT, WIDTH = 640, 640
 
 def representative_dataset_gen():
- for f_name in os.listdir(images_path):
-    file_path = os.path.normpath(os.path.join(images_path, f_name))
-    img = cv2.imread(file_path)
-    img = cv2.resize(img, (NORM_H, NORM_W))
-    img = np.reshape(img, (1, NORM_H, NORM_W, 3))
-    img = np.asarray(img, dtype='float32')
-    img /= 255.0
-    yield[img]
+   for image_path in rep_ds:
+       img = tf.io.read_file(image_path)
+       img = tf.io.decode_image(img, channels=3)
+       img = tf.image.convert_image_dtype(img, tf.float32)
+       resized_img = tf.image.resize(img, (HEIGHT, WIDTH))
+       resized_img = resized_img[tf.newaxis, :]
+       yield [resized_img]
 
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.allow_custom_ops = True
+model_path = './exported-models/ptag-detector-model/ssd_mobilenet_v2_fpnlite_640x640/saved_model'        
+
+#import trained model from mobilenet 640 v2 fpn
+converter = tf.lite.TFLiteConverter.from_saved_model(model_path) #using tensorflow
+
 converter.representative_dataset = representative_dataset_gen
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.inference_input_type = tf.int8  # or tf.int8
-converter.inference_output_type = tf.int8  # or tf.int8
-converter.experimental_new_converter = True
-tflite_model = converter.convert()
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+converter.quantized_input_stats = {"normalized_input_image_tensor": (128, 128)}
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+tflite_model_quant = converter.convert()
+
+interpreter = tf.lite.Interpreter(model_content=tflite_model_quant)
+input_type = interpreter.get_input_details()[0]['dtype']
+print('input: ', input_type)
+output_type = interpreter.get_output_details()[0]['dtype']
+print('output: ', output_type)
 
 # Save the model.
-with open('detect.tflite', 'wb') as f:
-  f.write(tflite_model)
+with open('detect_quant.tflite', 'wb') as f:
+  f.write(tflite_model_quant)
